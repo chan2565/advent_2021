6ad967a9e225f90dba16d80eca212d678c36903b
chan2565 <1981757+chan2565@users.noreply.github.com>
2021-12-15 11:10:24 -0500

    Day 4 Complete!

---
+++ day4_part1.py
@@ -0,0 +1,82 @@
+with open("day4_input.txt") as file:
+    lines = file.readlines()
+
+#with open("day4_ex_input.txt") as file:
+#    lines = file.readlines()
+
+draws = lines[0].strip().split(",")
+all_boards = lines[2:]
+all_boards.append("\n")
+board_list = []
+
+class BingoBoard:
+    def __init__(self, rows):
+        self.rows = rows
+        self.marks = [["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""]]
+    
+    def mark_board(self, num):
+        curr_row = 0
+        curr_col = 0
+        for row in self.rows:
+            for space in row:
+                if num == space:
+                    self.marks[curr_row][curr_col] = "x"
+                curr_col += 1
+            curr_col = 0
+            curr_row += 1
+    
+    def calc_unmarked(self):
+        sum = 0
+        curr_row = 0
+        curr_col = 0
+        for row in self.marks:
+            for space in row:
+                if space == "":
+                    sum += int(self.rows[curr_row][curr_col])
+                curr_col += 1
+            curr_col = 0
+            curr_row +=1
+        return sum
+
+    def check_board(self):
+        # Check columns
+        curr_row = 0
+        curr_col = 0
+        while curr_col < len(self.marks[0]):
+            if curr_row >= len(self.marks):
+                return self.calc_unmarked()
+            elif self.marks[curr_row][curr_col] == "x":
+                curr_row += 1
+                continue
+            else:
+                curr_col += 1
+                curr_row = 0
+
+        # Check rows
+        for row in self.marks:
+            if row[0] == "x" and row[1] == "x" and row[2] == "x" and row[3] == "x" and row[4] == "x":
+                return self.calc_unmarked()
+        
+        # Return 0 if no bingo
+        return 0
+
+def load_boards():
+    curr_board = []
+    for line in all_boards:
+        if line == "\n":
+            new_board = BingoBoard(curr_board)
+            board_list.append(new_board)
+            curr_board = []
+        else:
+            curr_board.append(line.strip().split())
+
+
+def mark_boards():
+    for num in draws:
+        for board in board_list:
+            board.mark_board(num)
+            result = board.check_board()
+            if result > 0:
+                print(f"sum unmarked = {result} current num = {num}")
+                print(f"{result} * {num} = {result * int(num)}")
+                return result * int(num)
